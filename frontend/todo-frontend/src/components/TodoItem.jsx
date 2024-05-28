581ba775b64f91f41e3f3e1c396cfa3c8d0367a9
shubhampatil <shubhampatil871@gmail.comgit config --global user.email shubhampatil871@gmail.com>
2024-05-29 00:45:17 +0530

    adjust tailwind classes

--- frontend/todo-frontend/src/components/TodoItem.jsx
+++ frontend/todo-frontend/src/components/TodoItem.jsx
@@ -10,24 +10,26 @@ const TodoItem = ({ todo, onDelete, onEdit }) => {
   };
 
   return (
-    <li className="todo-item">
+    <li className="todo-item mt-2">
       <h2 className="text-white text-xl font-semibold">{todo.title}</h2>
       <p className="p-1 w-96">{todo.description}</p>
-      <p className="badge badge-secondary">
-        Completed: {todo.completed ? "Yes" : "No"}
-      </p>
-      <button
-        className="btn btn-xs btn-outline btn-error mx-10"
-        onClick={handleDelete}
-      >
-        X
-      </button>
-      <button
-        className="btn btn-xs btn-outline btn-primary mx-10"
-        onClick={() => onEdit(todo)}
-      >
-        Edit
-      </button>
+      <div className="flex mt-4">
+        <p className="btn btn-xs btn-outline  mr-48">
+          Completed: {todo.completed ? "Yes" : "No"}
+        </p>
+        <button
+          className="btn btn-xs btn-outline btn-primary mr-4"
+          onClick={() => onEdit(todo)}
+        >
+          Edit
+        </button>
+        <button
+          className="btn btn-xs btn-outline btn-error flex-end "
+          onClick={handleDelete}
+        >
+          X
+        </button>
+      </div>
       <div className="divider"></div>
     </li>
   );
